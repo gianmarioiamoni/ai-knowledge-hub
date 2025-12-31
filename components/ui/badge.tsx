92c277283f4da4c24be95f8574e02274107accda
Gianmario Iamoni <gianmarioiamoni1@gmail.com>
2025-12-31 19:16:36 +0100

    fix: add JSX import to badge component

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import type { JSX } from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
 import { cn } from "@/lib/utils";
